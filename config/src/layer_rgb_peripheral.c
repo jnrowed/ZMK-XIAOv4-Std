28997b9bb62b5d69258f0714116bb07a4d0685e3
jnrowed <jnrowe.d@gmail.com>
2026-04-11 20:59:36 +0900

    Update layer_rgb_peripheral.c

--- config/src/layer_rgb_peripheral.c
+++ config/src/layer_rgb_peripheral.c
@@ -1,14 +1,22 @@
 #include <zephyr/kernel.h>
-#include <zmk/events/layer_state_changed.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gatt.h>
 #include <zmk/rgb_underglow.h>
-#include <zmk/keymap.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-static int layer_rgb_listener(const zmk_event_t *eh) {
-    uint8_t layer = zmk_keymap_highest_layer_active();
+static struct bt_uuid_128 lrgb_svc_uuid = BT_UUID_INIT_128(
+    0x73, 0x6b, 0x72, 0x65, 0x65, 0x2d, 0x6c, 0x72,
+    0x67, 0x62, 0x2d, 0x73, 0x76, 0x63, 0x00, 0x00
+);
+static struct bt_uuid_128 lrgb_chr_uuid = BT_UUID_INIT_128(
+    0x73, 0x6b, 0x72, 0x65, 0x65, 0x2d, 0x6c, 0x72,
+    0x67, 0x62, 0x2d, 0x63, 0x68, 0x72, 0x00, 0x00
+);
 
+static void apply_color(uint8_t layer)
+{
     switch (layer) {
         case 1:
             zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 60,  .s = 100, .b = 50});
@@ -29,8 +37,24 @@ static int layer_rgb_listener(const zmk_event_t *eh) {
         default:
             zmk_rgb_underglow_off(); break;
     }
-    return ZMK_EV_EVENT_BUBBLE;
 }
 
-ZMK_LISTENER(layer_rgb, layer_rgb_listener);
-ZMK_SUBSCRIPTION(layer_rgb, zmk_layer_state_changed);
+static ssize_t on_layer_written(struct bt_conn *conn,
+                                const struct bt_gatt_attr *attr,
+                                const void *buf, uint16_t len,
+                                uint16_t offset, uint8_t flags)
+{
+    if (len != 1) {
+        return BT_GATT_ERR(BT_ATT_ERR_INVALID_ATTRIBUTE_LEN);
+    }
+    apply_color(((const uint8_t *)buf)[0]);
+    return (ssize_t)len;
+}
+
+BT_GATT_SERVICE_DEFINE(lrgb_peripheral_svc,
+    BT_GATT_PRIMARY_SERVICE(&lrgb_svc_uuid),
+    BT_GATT_CHARACTERISTIC(&lrgb_chr_uuid.uuid,
+                           BT_GATT_CHRC_WRITE_WITHOUT_RESP,
+                           BT_GATT_PERM_WRITE,
+                           NULL, on_layer_written, NULL),
+);
