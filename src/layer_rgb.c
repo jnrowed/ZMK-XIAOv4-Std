bb0dac4fdfafad424e9650e4e166f79712bcb34d
jnrowed <jnrowe.d@gmail.com>
2026-04-11 15:44:38 +0900

    Update layer_rgb.c

--- src/layer_rgb.c
+++ src/layer_rgb.c
@@ -1,4 +1,5 @@
 #include <zephyr/kernel.h>
+#include <zephyr/init.h>
 #include <zmk/events/layer_state_changed.h>
 #include <zmk/rgb_underglow.h>
 #include <zmk/keymap.h>
@@ -6,33 +7,40 @@
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
+static int layer_rgb_init(void) {
+    zmk_rgb_underglow_off();
+    return 0;
+}
+
+SYS_INIT(layer_rgb_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
 static int layer_rgb_listener(const zmk_event_t *eh) {
     uint8_t layer = zmk_keymap_highest_layer_active();
 
     switch (layer) {
         case 1:
-            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 60,  .s = 100, .b = 50}); // Yellow
+            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 60,  .s = 100, .b = 50});
             zmk_rgb_underglow_on();
             break;
         case 2:
-            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 120, .s = 100, .b = 50}); // Green
+            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 120, .s = 100, .b = 50});
             zmk_rgb_underglow_on();
             break;
         case 3:
-            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 240, .s = 100, .b = 50}); // Blue
+            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 240, .s = 100, .b = 50});
             zmk_rgb_underglow_on();
             break;
         case 4:
-            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 0,   .s = 0,   .b = 20}); // Dim white
+            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 0,   .s = 0,   .b = 20});
             zmk_rgb_underglow_on();
             break;
         case 5:
         case 6:
-            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 0,   .s = 100, .b = 50}); // Red
+            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 0,   .s = 100, .b = 50});
             zmk_rgb_underglow_on();
             break;
         default:
-            zmk_rgb_underglow_off(); // Layer 0 = off
+            zmk_rgb_underglow_off();
             break;
     }
     return ZMK_EV_EVENT_BUBBLE;
