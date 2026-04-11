0ec45a9fdf8d478b90004cc1d679282dd15968e4
jnrowed <jnrowe.d@gmail.com>
2026-04-11 21:59:33 +0900

    Update layer_rgb_peripheral.c

--- config/src/layer_rgb_peripheral.c
+++ config/src/layer_rgb_peripheral.c
@@ -57,39 +57,4 @@ BT_GATT_SERVICE_DEFINE(lrgb_peripheral_svc,
                            BT_GATT_CHRC_WRITE_WITHOUT_RESP,
                            BT_GATT_PERM_WRITE,
                            NULL, on_layer_written, NULL),
-);#include <zephyr/kernel.h>
-#include <zmk/events/layer_state_changed.h>
-#include <zmk/rgb_underglow.h>
-#include <zmk/keymap.h>
-#include <zephyr/logging/log.h>
-
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
-
-static int layer_rgb_listener(const zmk_event_t *eh) {
-    uint8_t layer = zmk_keymap_highest_layer_active();
-
-    switch (layer) {
-        case 1:
-            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 60,  .s = 100, .b = 50});
-            zmk_rgb_underglow_on();  break;
-        case 2:
-            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 120, .s = 100, .b = 50});
-            zmk_rgb_underglow_on();  break;
-        case 3:
-            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 240, .s = 100, .b = 50});
-            zmk_rgb_underglow_on();  break;
-        case 4:
-            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 0,   .s = 0,   .b = 25});
-            zmk_rgb_underglow_on();  break;
-        case 5:
-        case 6:
-            zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 0,   .s = 100, .b = 50});
-            zmk_rgb_underglow_on();  break;
-        default:
-            zmk_rgb_underglow_off(); break;
-    }
-    return ZMK_EV_EVENT_BUBBLE;
-}
-
-ZMK_LISTENER(layer_rgb, layer_rgb_listener);
-ZMK_SUBSCRIPTION(layer_rgb, zmk_layer_state_changed);
+);
