b18a2a3f7d88320a89eb58c3824186e47f5b723d
jnrowed <jnrowe.d@gmail.com>
2026-04-11 22:25:01 +0900

    Update layer_rgb_peripheral.c

--- config/src/layer_rgb_peripheral.c
+++ config/src/layer_rgb_peripheral.c
@@ -15,8 +15,18 @@ static struct bt_uuid_128 lrgb_chr_uuid = BT_UUID_INIT_128(
     0x67, 0x62, 0x2d, 0x63, 0x68, 0x72, 0x00, 0x00
 );
 
-static void apply_color(uint8_t layer)
-{
+/* Blink n times in given color, then go back off */
+static void blink(uint16_t h, uint8_t s, uint8_t b, int times) {
+    for (int i = 0; i < times; i++) {
+        zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = h, .s = s, .b = b});
+        zmk_rgb_underglow_on();
+        k_sleep(K_MSEC(200));
+        zmk_rgb_underglow_off();
+        k_sleep(K_MSEC(200));
+    }
+}
+
+static void apply_color(uint8_t layer) {
     switch (layer) {
         case 1:
             zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = 60,  .s = 100, .b = 50});
@@ -47,6 +57,10 @@ static ssize_t on_layer_written(struct bt_conn *conn,
     if (len != 1) {
         return BT_GATT_ERR(BT_ATT_ERR_INVALID_ATTRIBUTE_LEN);
     }
+
+    /* White blink = write arrived */
+    blink(0, 0, 80, 1);
+
     apply_color(((const uint8_t *)buf)[0]);
     return (ssize_t)len;
 }
@@ -58,3 +72,14 @@ BT_GATT_SERVICE_DEFINE(lrgb_peripheral_svc,
                            BT_GATT_PERM_WRITE,
                            NULL, on_layer_written, NULL),
 );
+
+/* Boot blink: 3x cyan = peripheral is alive and service is registered */
+static int lrgb_periph_boot(void) {
+    k_sleep(K_SECONDS(2)); /* wait for RGB to initialize */
+    blink(180, 100, 80, 3);
+    return 0;
+}
+
+static K_THREAD_DEFINE(lrgb_boot_tid, 512,
+                       lrgb_periph_boot, NULL, NULL, NULL,
+                       K_LOWEST_APPLICATION_THREAD_PRIO, 0, 0);
