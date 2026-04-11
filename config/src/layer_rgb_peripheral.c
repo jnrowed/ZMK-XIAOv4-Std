9fe4306ad48063e13aa75ffd3a482cf73e9b1ffa
jnrowed <jnrowe.d@gmail.com>
2026-04-12 00:21:04 +0900

    Update layer_rgb_peripheral.c

--- config/src/layer_rgb_peripheral.c
+++ config/src/layer_rgb_peripheral.c
@@ -2,27 +2,24 @@
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/gatt.h>
 #include <zmk/rgb_underglow.h>
+#include <zmk/event_manager.h>
+#include <zmk/events/split_peripheral_status_changed.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-static struct bt_uuid_128 lrgb_svc_uuid = BT_UUID_INIT_128(
-    BT_UUID_128_ENCODE(0x6c726762, 0x2d73, 0x7663, 0x2d00, 0x000000000000)
-);
-static struct bt_uuid_128 lrgb_chr_uuid = BT_UUID_INIT_128(
-    BT_UUID_128_ENCODE(0x6c726762, 0x2d63, 0x6872, 0x2d00, 0x000000000000)
-);
-
-/* Blink n times in given color, then go back off */
-static void blink(uint16_t h, uint8_t s, uint8_t b, int times) {
-    for (int i = 0; i < times; i++) {
-        zmk_rgb_underglow_set_hsb((struct zmk_led_hsb){.h = h, .s = s, .b = b});
-        zmk_rgb_underglow_on();
-        k_sleep(K_MSEC(200));
-        zmk_rgb_underglow_off();
-        k_sleep(K_MSEC(200));
-    }
-}
+static struct bt_uuid_128 lrgb_svc_uuid = {
+    .uuid = { BT_UUID_TYPE_128 },
+    .val = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+             0x00, 0x2d, 0x63, 0x76, 0x73, 0x2d,
+             0x62, 0x67, 0x72, 0x6c }
+};
+static struct bt_uuid_128 lrgb_chr_uuid = {
+    .uuid = { BT_UUID_TYPE_128 },
+    .val = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+             0x00, 0x2d, 0x72, 0x68, 0x63, 0x2d,
+             0x62, 0x67, 0x72, 0x6c }
+};
 
 static void apply_color(uint8_t layer) {
     switch (layer) {
@@ -50,15 +47,10 @@ static void apply_color(uint8_t layer) {
 static ssize_t on_layer_written(struct bt_conn *conn,
                                 const struct bt_gatt_attr *attr,
                                 const void *buf, uint16_t len,
-                                uint16_t offset, uint8_t flags)
-{
+                                uint16_t offset, uint8_t flags) {
     if (len != 1) {
         return BT_GATT_ERR(BT_ATT_ERR_INVALID_ATTRIBUTE_LEN);
     }
-
-    /* White blink = write arrived */
-    blink(0, 0, 80, 1);
-
     apply_color(((const uint8_t *)buf)[0]);
     return (ssize_t)len;
 }
@@ -67,17 +59,14 @@ BT_GATT_SERVICE_DEFINE(lrgb_peripheral_svc,
     BT_GATT_PRIMARY_SERVICE(&lrgb_svc_uuid),
     BT_GATT_CHARACTERISTIC(&lrgb_chr_uuid.uuid,
                            BT_GATT_CHRC_WRITE_WITHOUT_RESP,
-                           BT_GATT_PERM_WRITE_ENCRYPT,
+                           BT_GATT_PERM_WRITE,
                            NULL, on_layer_written, NULL),
 );
 
-/* Boot blink: 3x cyan = peripheral is alive and service is registered */
-static int lrgb_periph_boot(void) {
-    k_sleep(K_SECONDS(2)); /* wait for RGB to initialize */
-    blink(180, 100, 80, 3);
-    return 0;
+static int periph_status_listener(const zmk_event_t *eh) {
+    zmk_rgb_underglow_off();
+    return ZMK_EV_EVENT_BUBBLE;
 }
 
-static K_THREAD_DEFINE(lrgb_boot_tid, 512,
-                       lrgb_periph_boot, NULL, NULL, NULL,
-                       K_LOWEST_APPLICATION_THREAD_PRIO, 0, 0);
+ZMK_LISTENER(lrgb_periph_status, periph_status_listener);
+ZMK_SUBSCRIPTION(lrgb_periph_status, zmk_split_peripheral_status_changed);
